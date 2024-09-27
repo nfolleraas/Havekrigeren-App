de5fb85315e226eb007a5b8efe1de52c2c7ca96d
nfolleraas <nfolleraas@gmail.com>
2024-09-27 16:53:59 +0200

    added files

---
+++ HavekrigerenApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace HavekrigerenApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            Console.WriteLine("App is starting up");
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+
+            Console.WriteLine("App is going to sleep");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Console.WriteLine("App is resuming");
+        }
+
+    }
+}
---
+++ HavekrigerenApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HavekrigerenApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
