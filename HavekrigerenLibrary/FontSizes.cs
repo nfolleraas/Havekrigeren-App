86610c991c4e55660f8aa6e9ab0a7cc04d370ca1
nfolleraas <nfolleraas@gmail.com>
2024-10-28 15:17:06 +0100

    changes

---
+++ HavekrigerenLibrary/FontSizes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HavekrigerenLibrary
+{
+    public static class FontSizes
+    {
+        public static double Small { get; } = 12.0;
+        public static double Medium { get; } = 16.0;
+        public static double Large { get; } = 20.0;
+    }
+}
